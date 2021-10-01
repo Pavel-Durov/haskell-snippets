5b18c9e71332f1b673dc39382a4264c28ee19e2d
Pavel Durov <pav3ldurov@gmail.com>
2021-10-01 11:11:24 +0100

    chore: swith ' fibo func name

--- snippets/fibo.hs
+++ snippets/fibo.hs
@@ -1,16 +1,16 @@
 import Lib
 
 
-fibo' :: Int -> [Int]
-fibo' 1 = [1]
-fibo' 2 = [1,1]
-fibo' x = fibo(x-1) ++ [ ((last . fibo)(x-1)) + ((last . init . fibo)(x-1)) ]
-
--- using http://learnyouahaskell.com/syntax-in-functions
 fibo :: Int -> [Int]
 fibo 1 = [1]
 fibo 2 = [1,1]
-fibo x = 
+fibo x = fibo(x-1) ++ [ ((last . fibo)(x-1)) + ((last . init . fibo)(x-1)) ]
+
+-- using http://learnyouahaskell.com/syntax-in-functions
+fibo' :: Int -> [Int]
+fibo' 1 = [1]
+fibo' 2 = [1,1]
+fibo' x = 
     let next = pred x
         fst = last (fibo (next)) -- (last . fibo) x1 
         snd = last (init (fibo next)) -- (last . init . fibo) x1
